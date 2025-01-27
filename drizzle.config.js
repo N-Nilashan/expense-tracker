3f8ad16dbb841bb4078133cb673383b478306587
N-Nilashan <informal.nimesh@gmail.com>
2025-01-28 01:34:13 +0530

    updated files

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,7 +1,18 @@
-export default {
-    schema: "./utils/schema.jsx",
-    driver: 'pg',
-    dbCredentials: {
-        connectionString: process.env.DATABASE_URL,
-    }
-};
+// export default {
+//     schema: "./utils/schema.jsx",
+//     driver: 'pg',
+//     dbCredentials: {
+//         connectionString:process.env.DATABASE_URL,
+//     }
+// };
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  dialect: "postgresql",
+  schema: "./utils/schema.jsx",
+    out: "./drizzle",
+   driver: "pglite",
+  dbCredentials: {
+  url:'process.env.NEXT_PUBLIC_DATABASE_URL',
+  },
+});
